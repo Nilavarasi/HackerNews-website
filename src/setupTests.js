2d2de15393d849aa91ad9e964faee20ab1632190
Nilavarasi <nilas116@gmail.com>
2021-08-05 21:25:29 +0200

    Add basic test

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,19 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+// import '@testing-library/jest-dom';
+import {toBeInTheDocument,toHaveStyle} from 
+  '@testing-library/jest-dom';
+import { render, getByText } from '@testing-library/react';
+import App from './App';
+
+expect.extend({ toBeInTheDocument, toHaveStyle });
+
+ 
+ test('Test for loading state', () => {
+     render(
+         <App />
+     )
+     const loadingElem = getByText('Loading');
+     expect(loadingElem).toBeInTheDocument();
+ });
