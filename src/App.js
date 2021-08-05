7f6eb089db46bb466c11268b4f1acd569f764d10
Nilavarasi <nilas116@gmail.com>
2021-08-05 18:52:47 +0200

    Add styled components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
 import React from 'react';
 import axios from 'axios';
-import { Card } from 'semantic-ui-react'
+import moment from 'moment';
+import { Card, Image } from 'semantic-ui-react';
+import {
+  StyledCard,
+  TableRows
+} from './style';
 
 export default class App extends React.Component {
   state = {
@@ -11,35 +16,54 @@ export default class App extends React.Component {
   componentDidMount() {
     axios.get(`https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty`)
       .then(latest_news => {
-        latest_news.data.map(news_id => {
+        latest_news.data.map(news_id => 
           axios.get(`https://hacker-news.firebaseio.com/v0/item/${news_id}.json?print=pretty`)
           .then(news => 
             this.setState({
               latestNews: [...this.state.latestNews, news['data']]
             })
           )
-        }) 
+        ) 
       })
   }
 
   render() {
+    const getTime = (timestamp) => {
+      const dateTimeAgo = moment.unix(timestamp).format("YYYY-MM-DD HH:mm:ss")
+      return moment(dateTimeAgo).fromNow();
+    }
+
     return (
       <React.Fragment>
-      <div className="News Container">
-        <Card fluid key="header" color="red" header="Hackerrank News"/>
+        <StyledCard>
+          <Card fluid key="header">
+          <Card.Content>
+            <div>
+              <Image 
+              src='https://pbs.twimg.com/profile_images/378800000011494576/9c90acb704cbf9eef6135009c9bb5657_400x400.png' />
+            </div>
+            <Card.Header>Hackerrank News</Card.Header>
+          </Card.Content>
+          </Card>
+          <br/>
+        </StyledCard>
         {
           this.state.latestNews.length > 30 &&
             this.state.latestNews
             .slice(0, this.state.current_page*30)
             .map(news => (
-              <Card.Group>
-                <Card fluid key={news.id} color='#ff6600'>
-                  <p>{news.title}</p>
-                </Card>
-              </Card.Group>
+              <TableRows>
+                <Card.Group>
+                  {console.log(news)}
+                  <Card fluid key={news.id}>
+                    <a href={news.url}>{news.title}</a>
+                    <p>{news.descendants} points by {news.by} {getTime(news.time)}</p>
+                    
+                  </Card>
+                </Card.Group>
+              </TableRows>
           ))
-        }
-      </div>
+        } 
       </React.Fragment>
     );
   } 
---
+++ src/style.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const StyledCard = styled.div`
+  background-color: white;
+  font-size: 32px;
+  color: black;
+  img {
+      width: 40px;
+      height: 40px;
+      float: left;
+  }
+  .content {
+      background-color: #ff6600!important;
+  }
+`;
+
+export const TableRows = styled.div`
+    color:  black;
+    width: 90%;
+    margin-left: 20px;
+    margin-right: 20px;
+`;
